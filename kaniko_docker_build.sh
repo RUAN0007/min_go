c374c3a9004fd3aa785f110f94fc5da98b0cffc2
ruanpc <ruanpingcheng@gmail.com>
2025-04-12 14:25:27 +0800

    add kaniko build

--- kaniko_docker_build.sh
+++ kaniko_docker_build.sh
@@ -1,4 +1,5 @@
 docker run --rm \
+  --platform linux/amd64 \
   -v $(pwd):/workspace \
   gcr.io/kaniko-project/executor@sha256:9e69fd4330ec887829c780f5126dd80edc663df6def362cd22e79bcdf00ac53f \
   --dockerfile=Dockerfile \
