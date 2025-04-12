fda41dc4a846fb8aa25d8005e0896ed8051afa9f
ruanpc <ruanpingcheng@gmail.com>
2025-04-12 14:19:44 +0800

    add kaniko related

---
+++ kaniko_docker_build.sh
@@ -0,0 +1,10 @@
+docker run --rm \
+  -v $(pwd):/workspace \
+  gcr.io/kaniko-project/executor@sha256:9e69fd4330ec887829c780f5126dd80edc663df6def362cd22e79bcdf00ac53f \
+  --dockerfile=Dockerfile \
+  --context=dir:///workspace \
+  --destination=kaniko_enclave:latest \
+  --no-push \
+  --tarPath=/workspace/enclave_kaniko.tar \
+  --reproducible \
+  --single-snapshot
