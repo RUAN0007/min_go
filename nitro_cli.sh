36afde07d0a2755515b4034a05ef6349f8f0988a
ruanpc <ruanpingcheng@gmail.com>
2025-04-12 15:35:00 +0800

    confirm identical pcr built from mac and linux

---
+++ nitro_cli.sh
@@ -0,0 +1,7 @@
+docker run \
+    --rm \
+    --platform linux/amd64 \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ./from_personal_mac/enclave_kaniko.tar:/kaniko.tar \
+    amazonlinxwithnitrocli:latest \
+    -c 'docker load -i kaniko.tar && nitro-cli build-enclave --docker-uri kaniko_enclave:latest --output-file enclave.eif'
